9c058562fe78282c8536088de43f9d6ad58cf2ff
aline-fagundes <aline.martinsnapoli@gmail.com>
2022-10-22 11:14:32 -0300

    Adicionando projeto de telefone

---
+++ main.js
@@ -0,0 +1,26 @@
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+
+  tecla.onclick = function () {
+    inputTel.value = inputTel.value + tecla.value;
+  }
+}
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  
+    const tecla = listaDeTeclas[contador];
+
+  tecla.onkeydown = function (evento) {
+    if(evento.code === "Enter" || evento.code === "Space") {
+        tecla.classList.add('ativa');
+    }
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+}
